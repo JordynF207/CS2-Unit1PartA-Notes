e6bffddb4622d823b081a09f695f8f8e3c0527d3
JordynF207 <Jordyn.friedlander2027@gbwl.org>
2026-09-18 17:56:33 +0000

    debugged

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main {
 /* 
 This is my comment space! 
+
 */
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("It makes no sense to divide a number by zero!");
+      System.out.println(0/3);
    }
+
 }
